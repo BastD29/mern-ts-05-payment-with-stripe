d09e5621e92480b6acbef1c417de2228bcef103c
BastD29 <bastiendaniel9@gmail.com>
2024-07-19 19:53:01 +0200

    added commented session object example

--- server/src/controllers/session.ts
+++ server/src/controllers/session.ts
@@ -36,9 +36,78 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     });
     console.log("session:", session);
 
+    // session: {
+    //   id: 'cs_test_b1qz7k1X0m3nzSB1HiFAXx1vw6J6rH6UhAEG7RJgq7CG9Y7pUb4FIYvmPx',
+    //   object: 'checkout.session',
+    //   after_expiration: null,
+    //   allow_promotion_codes: null,
+    //   amount_subtotal: 50000,
+    //   amount_total: 50000,
+    //   automatic_tax: { enabled: false, liability: null, status: null },
+    //   billing_address_collection: null,
+    //   cancel_url: 'http://localhost:5173/cancel.html',
+    //   client_reference_id: null,
+    //   client_secret: null,
+    //   consent: null,
+    //   consent_collection: null,
+    //   created: 1721410821,
+    //   currency: 'usd',
+    //   currency_conversion: null,
+    //   custom_fields: [],
+    //   custom_text: {
+    //     after_submit: null,
+    //     shipping_address: null,
+    //     submit: null,
+    //     terms_of_service_acceptance: null
+    //   },
+    //   customer: null,
+    //   customer_creation: 'if_required',
+    //   customer_details: null,
+    //   customer_email: null,
+    //   expires_at: 1721497221,
+    //   invoice: null,
+    //   invoice_creation: {
+    //     enabled: false,
+    //     invoice_data: {
+    //       account_tax_ids: null,
+    //       custom_fields: null,
+    //       description: null,
+    //       footer: null,
+    //       issuer: null,
+    //       metadata: {},
+    //       rendering_options: null
+    //     }
+    //   },
+    //   livemode: false,
+    //   locale: null,
+    //   metadata: {},
+    //   mode: 'payment',
+    //   payment_intent: null,
+    //   payment_link: null,
+    //   payment_method_collection: 'if_required',
+    //   payment_method_configuration_details: null,
+    //   payment_method_options: { card: { request_three_d_secure: 'automatic' } },
+    //   payment_method_types: [ 'card' ],
+    //   payment_status: 'unpaid',
+    //   phone_number_collection: { enabled: false },
+    //   recovered_from: null,
+    //   saved_payment_method_options: null,
+    //   setup_intent: null,
+    //   shipping_address_collection: null,
+    //   shipping_cost: null,
+    //   shipping_details: null,
+    //   shipping_options: [],
+    //   status: 'open',
+    //   submit_type: null,
+    //   subscription: null,
+    //   success_url: 'http://localhost:5173/success.html',
+    //   total_details: { amount_discount: 0, amount_shipping: 0, amount_tax: 0 },
+    //   ui_mode: 'hosted',
+    //   url: 'https://checkout.stripe.com/c/pay/cs_test_b1qz7k1X0m3nzSB1HiFAXx1vw6J6rH6UhAEG7RJgq7CG9Y7pUb4FIYvmPx#fidkdWxOYHwnPyd1blpxYHZxWjA0VTJPTmtAajVsTzNLYWliVG5iVWNkXWE2PDJTf1UxYmJmMUhDQzFfdDRPU0tURn08M1Ywcl1hTGZDS21UYFFnVTd3M0pDMTdKcFxqU3FUR2h%2FZktwb3wxNTVoQWt1UnMycCcpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPydocGlxbFpscWBoJyknYGtkZ2lgVWlkZmBtamlhYHd2Jz9xd3BgeCUl'
+    // }
+
     res.json({ url: session.url });
   } catch (error) {
-    // res.status(500).json({ error: error.message });
     res.status(500).json({ error: (error as Error).message });
   }
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,7 @@
 import express from "express";
 import session from "./routes/session";
 import cors from "cors";
-import {
-  ALLOWED_ORIGIN,
-  NODE_ENV,
-  PORT /* STRIPE_PRIVATE_KEY */,
-} from "./config/environments";
-// import Stripe from "stripe";
-
-// if (!STRIPE_PRIVATE_KEY) {
-//   throw new Error("Stripe private key is not defined");
-// }
+import { ALLOWED_ORIGIN, NODE_ENV, PORT } from "./config/environments";
 
 const app = express();
 
@@ -32,13 +23,6 @@ const corsOptions: cors.CorsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
-
-// const stripe = new Stripe(STRIPE_PRIVATE_KEY);
-
-// const storeItems = new Map([
-//   [1, { priceInCents: 10000, name: "Learn React Today" }],
-//   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-// ]);
 
 // app.use("/api/sessions", session);
 app.use("/", session);
