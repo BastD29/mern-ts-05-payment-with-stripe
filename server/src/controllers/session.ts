22e177a07c92fba82c088bbe57900f9999b23f58
BastD29 <bastiendaniel9@gmail.com>
2024-07-19 19:36:59 +0200

    updated .gitignore

--- server/src/controllers/session.ts
+++ server/src/controllers/session.ts
@@ -11,11 +11,15 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     }
 
     const stripe = new Stripe(STRIPE_PRIVATE_KEY);
+    // console.log("stripe:", stripe);
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       line_items: req.body.items.map((item: ItemType) => {
         const storeItem = storeItems.get(item.id);
+        // console.log("storeItem:", storeItem);
+
         return {
           price_data: {
             currency: "usd",
@@ -30,6 +34,8 @@ const createCheckoutSession = async (req: Request, res: Response) => {
       success_url: `${CLIENT_URL}/success.html`,
       cancel_url: `${CLIENT_URL}/cancel.html`,
     });
+    console.log("session:", session);
+
     res.json({ url: session.url });
   } catch (error) {
     // res.status(500).json({ error: error.message });
--- server/src/routes/session.ts
+++ server/src/routes/session.ts
@@ -3,9 +3,10 @@ import { createCheckoutSession } from "../controllers/session";
 
 const router = express.Router();
 
-// router.post("/create-checkout-session", createCheckoutSession);
-router.post("/create-checkout-session", (req, res) => {
-  res.json({ url: "Hi" });
-});
+router.post("/create-checkout-session", createCheckoutSession);
+
+// router.post("/create-checkout-session", (req, res) => {
+//   res.json({ url: "Hi" });
+// });
 
 export default router;
